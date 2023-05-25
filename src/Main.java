8f9b2225312ede16386728b79123bddb4ed54fda
Kseniya Radiseva <Kotaviy@gmail.com>
2023-05-25 23:21:41 +0500

    Code reformated

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int count = 300; //сумма на счёте
-        int sum = 1200; // сумма, внесённая на счёт
-        int bonusRUB = sum / 100; // бонусные рубли
-        int finalCount = count + sum; // счёт после внесения суммы
-            if(sum > 1000) { // если внесённая сумма больше 1000 рублей
-               count = finalCount + bonusRUB; // то на счёт зачисляются бонусные рубли
-            } else { // если сумма меньше 1000 рублей
-                bonusRUB = 0; // то бонусные рубли не начисляются
-            }
+        int count = 300;
+        int sum = 1200;
+        int bonusRUB = sum / 100;
+        int finalCount = count + sum;
+        if (sum > 1000) {
+            count = finalCount + bonusRUB;
+        } else {
+            bonusRUB = 0;
+        }
         System.out.println("На Вашем счёте " + finalCount + " рублей.");
         System.out.println("Бонус " + bonusRUB + " рублей.");
     }
